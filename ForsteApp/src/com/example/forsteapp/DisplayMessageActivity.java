fc47d6342868ef0c028d9b493d383866c67d0426
Simen Løkken <simen.lokken@gmail.com>
2012-09-11 22:02:03 +0200

    Wohoo
    Halfway through making a second activity.

---
+++ ForsteApp/src/com/example/forsteapp/DisplayMessageActivity.java
@@ -0,0 +1,17 @@
+package com.example.forsteapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class DisplayMessageActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        
+    }
+}
